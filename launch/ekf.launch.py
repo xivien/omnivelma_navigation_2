ca9862c778a15a2b25f61e0d8344530442b8a2f0
xivien <przemyslaw.szostak95@gmail.com>
2020-08-14 11:26:30 +0200

    test

---
+++ launch/ekf.launch.py
@@ -0,0 +1,37 @@
+from launch import LaunchDescription
+from ament_index_python.packages import get_package_share_directory
+import launch_ros.actions
+import os
+import yaml
+from launch.substitutions import EnvironmentVariable
+import pathlib
+import launch.actions
+from launch.actions import DeclareLaunchArgument
+
+
+def generate_launch_description():
+    ekf_odom_params = os.path.join(
+        get_package_share_directory('omnivelma_navigation_2'),
+        'params',
+        'ekf_odom.yaml')
+    ekf_map_params = os.path.join(
+        get_package_share_directory('omnivelma_navigation_2'),
+        'params',
+        'ekf_map.yaml')
+
+    return LaunchDescription([
+        launch_ros.actions.Node(
+            package='robot_localization',
+            executable='ekf_node',
+            name='ekf_odom_node',
+            output='screen',
+            parameters=[ekf_odom_params],
+        ),
+        # launch_ros.actions.Node(
+        #     package='robot_localization',
+        #     executable='ekf_node',
+        #     name='ekf_map_node',
+        #     output='screen',
+        #     parameters=[ekf_map_params],
+        # ),
+    ])
